786d87498a51e1d0fa4d975944c8caf705bda4c3
wy333 <1577653650@qq.com>
2019-07-11 22:26:37 +0800

    07-11

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,18 @@ export default new Router({
       path: '/login',
       component: () => import('@/views/test.vue')
     },
+    {
+      path: '/mail1',
+      component: () => import('@/views/mail/mail1.vue')
+    },
+    {
+      path: '/mail2',
+      component: () => import('@/views/mail/mail2.vue')
+    },
+    {
+      path: '/mail3',
+      component: () => import('@/views/mail/mail3.vue')
+    },
   ],
   linkActiveClass: 'active',
   linkExactActiveClass: 'active-exact'
